a66a3aa612f24b90c0ccccddf0a6b3295cd41a99
farel39 <argyafarel.kasyara@gmail.com>
2025-02-19 14:07:18 +0700

    Created Unit Test for main application

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,13 +1,22 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.Mockito.mock;
 
 @SpringBootTest
 class EshopApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testMain() {
+		SpringApplication mockSpringApp = mock(SpringApplication.class);
+		EshopApplication.main(new String[]{});
 	}
 
 }
