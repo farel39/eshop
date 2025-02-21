16fb29d187a9acbaafc95dba764a68673ab55745
farel39 <argyafarel.kasyara@gmail.com>
2025-02-21 13:28:16 +0700

    Fix maintainability issue where the imports that aren't used

--- src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/EshopApplicationTests.java
@@ -1,11 +1,9 @@
 package id.ac.ui.cs.advprog.eshop;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.mockito.Mockito.mock;
 
 @SpringBootTest
 class EshopApplicationTests {
--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomeControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomeControllerTest.java
@@ -1,10 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 
 class HomeControllerTest {
 
