190afebbd0b9215068509bf6a4b38bd02246451a
farel39 <argyafarel.kasyara@gmail.com>
2025-02-28 14:00:57 +0700

    Created an abstract base class Item in the model layer to accommodate OCP, refactor Car and Product to extend Item

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Car.java
@@ -1,11 +1,11 @@
 package id.ac.ui.cs.advprog.eshop.model;
+
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
-public class Car {
-    private String carId;
-    private String carName;
-    private String carColor;
-    private int carQuantity;
+@Getter
+@Setter
+public class Car extends Item {
+    // Additional attribute unique to Car
+    private String color;
 }
--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -3,9 +3,9 @@ package id.ac.ui.cs.advprog.eshop.model;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
-public class Product {
-    private String productId;
-    private String productName;
-    private int productQuantity;
+@Getter
+@Setter
+public class Product extends Item {
+    // Product-specific attributes (if any) can be added here
 }
+
