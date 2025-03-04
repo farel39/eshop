2042abf7ff2d89de523163aa25f99b97a937c0ba
farel39 <argyafarel.kasyara@gmail.com>
2025-03-04 12:09:47 +0700

    [GREEN] Implement Order model.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -4,6 +4,8 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Arrays;
 import java.util.List;
 
 @Builder
@@ -13,13 +15,41 @@ public class Order {
     List<Product> products;
     Long orderTime;
     String author;
-
-    @Setter
     String status;
 
     public Order(String id, List<Product> products, Long orderTime, String author) {
+        this.id = id;
+        this.orderTime = orderTime;
+        this.author = author;
+        this.status = "WAITING_PAYMENT";
+
+        if (products.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            this.products = products;
+        }
     }
 
     public Order(String id, List<Product> products, Long orderTime, String author, String status) {
+        this.id = id;
+        this.products = products;
+        this.orderTime = orderTime;
+        this.author = author;
+
+        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
+        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
+    }
+
+    public void setStatus(String status) {
+        String[] statusList = {"WAITING_PAYMENT", "FAILED", "SUCCESS", "CANCELLED"};
+        if (Arrays.stream(statusList).noneMatch(item -> item.equals(status))) {
+            throw new IllegalArgumentException();
+        } else {
+            this.status = status;
+        }
     }
 }
